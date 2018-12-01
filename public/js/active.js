5a128e42b99bdf295b4812c118c25ae2564d9b1f
Tomas Cerny <cernytomasj@gmail.com>
2018-12-01 15:21:38 +0100

    added node

--- public/js/active.js
+++ public/js/active.js
@@ -31,12 +31,12 @@
     $('#search-btn, #closeBtn').on('click', function () {
         $('body').toggleClass('search-form-on');
     });
-    
+
     // matchHeight Active Code
     if ($.fn.matchHeight) {
         $('.equal-height').matchHeight();
     }
-    
+
     // ScrollUp Active Code
     if ($.fn.scrollUp) {
         $.scrollUp({
@@ -82,4 +82,36 @@
         });
     });
 
+    var form = $('#register-form');
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        var formData = decodeURIComponent($(form).serialize().replace(/%2F/g, " ").replace('+', ' ')).split('&').map(input  => { const [key, value] = input.split('='); return {[key]: value} });
+
+        postData('/api/users', formData)
+    });
+
 })(jQuery);
+
+// postData(`http://example.com/answer`, {answer: 42})
+//     .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+//     .catch(error => console.error(error));
+
+function postData(url = ``, data = {}) {
+    // Default options are marked with *
+    return fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+        .then(response => response); // parses response to JSON
+}
