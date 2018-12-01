979c64b74f8653265c9f103b20180cac06d31d0d
Tomas Cerny <cernytomasj@gmail.com>
2018-12-01 16:35:49 +0100

    added profile

--- public/js/active.js
+++ public/js/active.js
@@ -89,10 +89,16 @@
 
         var formData = decodeURIComponent($(form).serialize().replace(/%2F/g, " ").replace('+', ' ')).split('&').map(input  => { const [key, value] = input.split('='); return {[key]: value} });
 
-        postData('/api/users', formData)
+        var response = postData('/api/users', formData).then(body => {
+            console.log(body)
+            window.localStorage.setItem('userId', body.userId);
+            window.location.replace('./personal.html')
+        })
+
     });
 
 })(jQuery);
+
 
 // postData(`http://example.com/answer`, {answer: 42})
 //     .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
@@ -113,5 +119,26 @@ function postData(url = ``, data = {}) {
         referrer: "no-referrer", // no-referrer, *client
         body: JSON.stringify(data), // body data type must match "Content-Type" header
     })
-        .then(response => response); // parses response to JSON
+        .then(response => response.json()); // parses response to JSON
+}
+
+function getData(url = ``) {
+    // Default options are marked with *
+    return fetch(url, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+    })
+        .then(response => response.json()); // parses response to JSON
+}
+
+function fetchUser(userId) {
+    return getData(`/api/users/${userId}`).then(response => response)
 }
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 let UsersRepository = [];
+const findUserByEmail = (email) => {
+    return UsersRepository.find(user => user.email === email)
+}
+const findUserById = (id) => {
+    return UsersRepository.find(user => user.id == id)
+}
+let ids = 1;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(UsersRepository);
+
+    res.send(UsersRepository);
+});
+
+router.get('/:userId', function(req, res, next) {
+
+    const user = findUserById(req.params.userId);
+    if(!user) {
+        res.sendStatus(204);
+        return next()
+    }
+    res.send(user);
 });
 
 router.post('/', (req, res, next) => {
     console.log(req.body);
-    UsersRepository.push(req.body);
+    if (findUserByEmail(req.body.email)) {
+        res.sendStatus(400)
+        return next()
+    }
+    const id = ids++;
+    UsersRepository.push(Object.assign({id}, ...req.body));
 
-    res.sendStatus(200);
+    res.send({userId: id});
 })
 
 module.exports = router;
